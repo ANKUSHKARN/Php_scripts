5052fe4773714ea087326f369088f45e3edbb3c4
Ankush kumar <118280532+ANKUSHKARN@users.noreply.github.com>
2024-11-20 14:53:46 +0530

    Update 2e4b2849game.php

--- 2e4b2849game.php
+++ 2e4b2849game.php
@@ -1,85 +1,59 @@
-<?php
-// Demand a GET parameter
-if (!isset($_GET['name']) || strlen($_GET['name']) < 1) {
-    echo '<!DOCTYPE html>
-    <html>
-    <head><title>Please Log In</title></head>
-    <body>
-    <p><a href="index.php">Please Log In</a></p>
-    </body>
-    </html>';
+<?php // Do not put any HTML above this line
+if ( isset($_POST['cancel'] ) ) {
+    // Redirect the browser to game.php
+    header("Location: index.php");
     return;
 }
-
-// If the user requested logout, go back to index.php
-if (isset($_POST['logout'])) {
-    header('Location: index.php');
-    return;
-}
-
-// Set up the values for the game
-$names = array('Rock', 'Paper', 'Scissors');
-$human = isset($_POST['human']) ? $_POST['human'] + 0 : -1;
-$computer = rand(0, 2); // Randomize computer's choice
-
-// Function to determine the game result
-function check($computer, $human) {
-    if ($human == $computer) {
-        return "Tie";
-    } else if (($human == 1 && $computer == 0) || 
-               ($human == 2 && $computer == 1) || 
-               ($human == 0 && $computer == 2)) {
-        return "You Win";
+$salt = 'XyZzy12*_';
+$stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';  // Pw is php123
+$failure = false;  // If we have no POST data
+// Check to see if we have some POST data, if we do process it
+if ( isset($_POST['who']) && isset($_POST['pass']) ) {
+    if ( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1 ) {
+        $failure = "User name and password are required";
     } else {
-        return "You Lose";
+        $check = hash('md5', $salt.$_POST['pass']);
+        if ( $check == $stored_hash ) {
+            // Redirect the browser to game.php
+            header("Location: game.php?name=".urlencode($_POST['who']));
+            return;
+        } else {
+            $failure = "Incorrect password";
+        }
     }
 }
-
-// Check to see how the play happened
-$result = ($human == -1 || $human == 3) ? false : check($computer, $human);
+// Fall through into the View
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Ankush kumar 2e4b2849</title>
 <?php require_once "bootstrap.php"; ?>
+<title>Anamitra Musib 32ce3337</title>
 </head>
 <body>
 <div class="container">
-<h1>Rock Paper Scissors</h1>
+<h1>Please Log In</h1>
 <?php
-if (isset($_REQUEST['name'])) {
-    echo "<p>Welcome: " . htmlentities($_REQUEST['name']) . "</p>\n";
+// Note triple not equals and think how badly double
+// not equals would work here...
+if ( $failure !== false ) {
+    // Look closely at the use of single and double quotes
+    echo('<p style="color: red;">'.htmlentities($failure)."</p>\n");
 }
 ?>
-<form method="post">
-    <select name="human">
-        <option value="-1">Select</option>
-        <option value="0">Rock</option>
-        <option value="1">Paper</option>
-        <option value="2">Scissors</option>
-        <option value="3">Test</option>
-    </select>
-    <input type="submit" value="Play">
-    <input type="submit" name="logout" value="Logout">
+<form method="POST">
+<label for="nam">User Name</label>
+<input type="text" name="who" id="nam"><br/>
+<label for="id_1723">Password</label>
+<input type="text" name="pass" id="id_1723"><br/>
+<input type="submit" value="Log In">
+<input type="submit" name="cancel" value="Cancel">
 </form>
-
-<pre>
-<?php
-if ($human == -1) {
-    print "Please select a strategy and press Play.\n";
-} else if ($human == 3) {
-    for ($c = 0; $c < 3; $c++) {
-        for ($h = 0; $h < 3; $h++) {
-            $r = check($c, $h);
-            print "Human=$names[$h] Computer=$names[$c] Result=$r\n";
-        }
-    }
-} else {
-    print "Your Play=$names[$human] Computer Play=$names[$computer] Result=$result\n";
-}
-?>
-</pre>
+<p>
+For a password hint, view source and find a password hint
+in the HTML comments.
+<!-- Hint: The password is the four character sound a cat
+makes (all lower case) followed by 123. -->
+</p>
 </div>
 </body>
-</html>
